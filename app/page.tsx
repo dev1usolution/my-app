187fe6ce876f13d4efa2bd8f2bb8e56c531462ec
ari.utomo <itdev4.indorent@gmail.com>
2023-05-30 16:51:15 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-full flex justify-center items-center">
       <span className="text-white text-5xl text-bold">
-        Under Construction
+        Under Constructions
       </span>
     </div>
   )
